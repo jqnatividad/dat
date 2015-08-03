d0838d327160d55997069842a27e528ebe2a21cd
Mathias Buus <mathiasbuus@gmail.com>
2015-08-03 20:29:42 +0200

    resolve mdns dat names

--- bin/serve.js
+++ bin/serve.js
@@ -1,6 +1,9 @@
+var path = require('path')
 var http = require('http')
 var getport = require('getport')
 var pump = require('pump')
+var multicastdns = require('multicast-dns')
+var addr = require('network-address')
 var abort = require('../lib/util/abort.js')
 var openDat = require('../lib/util/open-dat.js')
 var usage = require('../lib/util/usage.js')('serve.txt')
@@ -29,12 +32,48 @@ function startDatServer (args) {
   if (args.help) return usage()
   if (args.port) return serve(parseInt(args.port, 10))
 
+  try {
+    var name = require(path.resolve(args.path, 'package.json')).name + '.dat'
+  } catch (err) {
+    // do nothing
+  }
+
   getport(6442, function (err, port) {
     if (err) abort(err, args)
     return serve(port)
   })
 
   function serve (port) {
+    var mdns = multicastdns()
+
+    mdns.on('error', function () {
+      // ignore errors
+    })
+
+    mdns.on('query', function (query) {
+      var qs = query.questions
+      for (var i = 0; i < qs.length; i++) {
+        if (qs[i].name.replace('.local', '') !== name) continue
+        if (qs[i].type !== 'A' && qs[i].type !== 'AAAA') continue
+
+        mdns.respond({
+          answers: [{
+            name: qs[i].name,
+            type: 'A',
+            ttl: 300,
+            data: addr.ipv4()
+          }],
+          additionals: [{
+            name: qs[i].name,
+            type: 'AAAA',
+            ttl: 300,
+            data: addr.ipv6()
+          }]
+        })
+        return
+      }
+    })
+
     openDat(args, function (err, db) {
       if (err) abort(err, args)
       if (!port) abort(new Error('Invalid port: ' + port), args)
